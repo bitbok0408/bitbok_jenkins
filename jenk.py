0a5ee1ec7bf432409f0cac3468b79571cea8c195
Viktor <Viktor.Kliui@gmail.com>
2017-08-03 12:05:04 +0300

    trytodo1

--- jenk.py
+++ jenk.py
@@ -4,3 +4,21 @@ print(os.environ['name'])
 
 
 print('ALL_test1', os.environ)
+
+
+
+# def auth_with():
+#     jenkins_param = os.environ['profile']
+#     profiles = {'user':('login', 'password'),
+#                 'root':('login', 'password'),
+#                 'administrator':('login', 'password'),
+#                 'supervisor':('login', 'password')}
+#     if jenkins_param in profiles.keys():
+#         return jenkins_param, profiles[jenkins_param]
+#     else:
+#         return jenkins_param, profiles['root']
+#
+#
+#
+# if jenkins_param == 'root':
+#     assert foo == True
